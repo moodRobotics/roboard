1c6adaadce7a16bb18cbf60dd59d4d889948df4f
moodrobotics <pablo@moodrob.com>
2020-12-13 00:30:05 +0100

    goto

--- README.md
+++ README.md
@@ -2,7 +2,20 @@
 
 Remote screen controled by robots. Create a cheap Videoconference, digital signage or dashboard with an old computer or raspberry pi connected to a small or large screen.
 
-You (or your robot) can change the screen content, change vol or Power On or Off with MQTT or HTPP commands.
+You (or your robot) can change the screen content, change vol or Power On or Off with MQTT or HTTP commands.
+
+## Commands
+
+You can use these commands:
+
+- **goto** *url* Where url is where the page it´s going to load
+- tv on
+- tv off
+- mute on
+- mute off
+- vol +
+- vol -
+- vol (0-10)
 
 ## Installation
 
@@ -17,11 +30,18 @@ Edit config.js and complete this data:
 - defaultViewport = null;
 - headless = false;
 - splashUrl = "https://tolentinoabogados.com";
+- mqtt = {
+    host: "mqtt://domain:PORT",
+    commandtopic: "/devices/command/nikola",
+    infotopic: "/devices/info/nikola"
+}
 
 ## Documentation
 
 To do this, it leverages the great work of:
 
-- [cec-monitor](https://github.com/senzil/cec-monitor) by Pablo González of Senzil, and
+- [cec-monitor](https://github.com/senzil/cec-monitor) by Pablo González of Senzil.
 - [libcec](https://github.com/Pulse-Eight/libcec) by PulseEight.
+- [hdmi-cec](https://github.com/jvanharn/node-hdmi-cec).
+- [node-cec](https://github.com/patlux/node-cec)
 - [MQTT](https://www.npmjs.com/package/mqtt).
--- roboard.js
+++ roboard.js
@@ -1,8 +1,9 @@
 const puppeteer = require('puppeteer')
 var mqtt = require('mqtt')
 const config = require('./config.js')
-const name = 'roboboard'
+const name = 'roboard'
 const version = '0.0.1a'
+let page
 
 //Open Chromium in full screen and load initial page
 async function start() {
@@ -12,26 +13,37 @@ async function start() {
     args: config.args,
   })
   let pages = await browser.pages()
-  const pagei = pages[0]
+  page = pages[0]
   //   const page = await browser.newPage()
-  await pagei.goto(config.splashUrl)
+  await page.goto(config.splashUrl)
 }
 
 if (config.mqtt.host) {
   var client = mqtt.connect(config.mqtt.host)
 
   client.on('connect', function () {
-    client.subscribe(config.mqtt.topic, function (err) {
+    client.subscribe(config.mqtt.commandtopic, function (err) {
       if (!err) {
-        client.publish(config.mqtt.topic, 'Hello mqtt')
+        client.publish(config.mqtt.infotopic, 'Hello mqtt')
       }
     })
   })
 
   client.on('message', function (topic, message) {
+    let command = message.toString().split(' ')[0].toLowerCase()
     // message is Buffer
-    console.log(message.toString())
+    console.log('command: ' + command + '  ' + message.toString())
     // client.end()
+    switch (command) {
+      case 'goto':
+        const url = message.toString().split(' ')[1]
+        client.publish(config.mqtt.infotopic, 'loading ' + url)
+        page
+          .goto(url)
+          .then(client.publish(config.mqtt.infotopic, 'loaded ' + url))
+        break
+      default:
+    }
   })
 }
 
